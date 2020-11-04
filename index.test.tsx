32c843e41bd2f138e0890be4168daf11a3c9bc07
jhsantana <henriique.ti@gmail.com>
2020-11-04 16:28:28 -0300

    1 file modified

--- index.test.tsx
+++ index.test.tsx
@@ -6,11 +6,13 @@ import Personagem from "./components/Personagem";
 import dummyData from "./components/Personagem/mock";
 
 describe("<Personagem />", () => {
-  it("should render the heading", () => {
+  it("should render the name of personagem", () => {
     dummyData.map((data, index) => {
       render(<Personagem {...data} key={index} />);
 
-      expect(screen.getByRole("heading", { name: data.name })).toBeTruthy();
+      expect(
+        screen.getByRole("heading", { name: data.name })
+      ).toBeInTheDocument();
     });
   });
 });
